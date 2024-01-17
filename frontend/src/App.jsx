c746ceda2216ff9800ea29b8619c87037d28db59
Muhammad Naufal Farras <opal.farras@gmail.com>
2024-01-17 16:29:28 +0700

    create footer section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
+import Footer from './components/footer/Footer';
 
 function App() {
-
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-500 font-bilbo">
         Hello world!
       </h1>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        'ubuntu': ["'Ubuntu', 'sans-serif'"],
-        'bilbo': ["'Bilbo Swash Caps', 'cursive'"] 
-      }
+        ubuntu: ["'Ubuntu', 'sans-serif'"],
+        bilbo: ["'Bilbo Swash Caps', 'cursive'"],
+      },
+      colors: {
+        footer: '#141414',
+      },
     },
   },
   plugins: [],
-}
-
+};
